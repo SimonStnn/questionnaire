2535e0a9bb6ba67569aeeea3c1ad4ca073057eec
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-26 16:56:03 +0200

    Write Question constructor test

---
+++ TestQuestionnaireLibrary/UnitTestQuestion.cs
@@ -0,0 +1,15 @@
+﻿using QuestionnaireLibrary;
+
+namespace TestQuestionnaireLibrary
+{
+	public class UnitTestQuestion
+	{
+		[Fact]
+		public void TestConstructor()
+		{
+			Question question = new("Are you reading this?");
+			Assert.NotNull(question);
+			Assert.Equal("Are you reading this?", question.Text);
+		}
+	}
+}
