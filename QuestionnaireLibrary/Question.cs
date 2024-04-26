8b8f0e64f5308f9b5ca5e03caef82553e3c58e92
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-26 23:29:59 +0200

    Refactor Question class in QuestionnaireLibrary

--- QuestionnaireLibrary/Question.cs
+++ QuestionnaireLibrary/Question.cs
@@ -6,33 +6,33 @@ using System.Threading.Tasks;
 
 namespace QuestionnaireLibrary
 {
-	public class Question
-	{
-		private readonly List<Answer> possibleAnser = new();
+    public class Question
+    {
+        private readonly List<Answer> possibleAnser = new();
 
-		public string Text { get; set; }
-		public string? ImageUrl { get; set; }
+        public string Text { get; set; }
+        public string? ImageUrl { get; set; }
 
-		public List<Answer> Answers => possibleAnser;
+        public List<Answer> Answers { get { return possibleAnser; } }
 
-		public Question(string text)
-		{
-			Text = text;
-		}
+        public Question(string text)
+        {
+            Text = text;
+        }
 
-		public void Add(Answer answer)
-		{
-			possibleAnser.Add(answer);
-		}
+        public void Add(Answer answer)
+        {
+            possibleAnser.Add(answer);
+        }
 
-		public Answer GetAnswer(int index)
-		{
-			return possibleAnser[index];
-		}
+        public Answer GetAnswer(int index)
+        {
+            return possibleAnser[index];
+        }
 
-		public override string ToString()
-		{
-			return Text;
-		}
-	}
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
 }
