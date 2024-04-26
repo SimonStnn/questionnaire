ac00f45fa39913df67b97890dcb15f21a2fecd5e
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-26 14:36:48 +0200

    Add Answer properties and constructor

--- QuestionnaireLibrary/Answer.cs
+++ QuestionnaireLibrary/Answer.cs
@@ -2,5 +2,13 @@
 {
 	public class Answer
 	{
+		public string Text;
+		public bool IsCorrect;
+
+		public Answer(string text, bool isCorrect)
+		{
+			Text = text;
+			IsCorrect = isCorrect;
+		}
 	}
 }
