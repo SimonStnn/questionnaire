0a32fae91bc908e31339e02201a4ebf07622a937
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-26 17:50:36 +0200

    Add Question.Add test

--- TestQuestionnaireLibrary/UnitTestQuestion.cs
+++ TestQuestionnaireLibrary/UnitTestQuestion.cs
@@ -4,12 +4,28 @@ namespace TestQuestionnaireLibrary
 {
 	public class UnitTestQuestion
 	{
+		Question question = new("Are you reading this?");
+
 		[Fact]
 		public void TestConstructor()
 		{
-			Question question = new("Are you reading this?");
-			Assert.NotNull(question);
-			Assert.Equal("Are you reading this?", question.Text);
+			Question question1 = new("Are you reading this?");
+			Assert.NotNull(question1);
+			Assert.Equal("Are you reading this?", question1.Text);
+			Assert.Throws<IndexOutOfRangeException>(() => question1.GetAnswer(0));
+		}
+
+		[Fact]
+		public void TestAdd()
+		{
+			Assert.Throws<IndexOutOfRangeException>(() => question.GetAnswer(0));
+			Answer answer = new("Some answer", true);
+			question.Add(answer);
+			Assert.Equal(answer, question.GetAnswer(0));
+			Answer answer2 = new("Some answer", false);
+			question.Add(answer2);
+			Assert.Equal(answer2, question.GetAnswer(1));
+			Assert.Throws<IndexOutOfRangeException>(() => question.GetAnswer(2));
 		}
 	}
 }
