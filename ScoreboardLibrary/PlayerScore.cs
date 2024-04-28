07952586cfe438fa65630c886077ac0f59d250e6
SimonStnn <simon.stijnen.23@gmail.com>
2024-04-28 14:56:51 +0200

    Add ScoreboardLibrary project

---
+++ ScoreboardLibrary/PlayerScore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardLibrary
+{
+    public class PlayerScore
+    {
+        private string name;
+        private int score;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public int Score
+        {
+            get => score;
+            set => score = value;
+        }
+
+        public PlayerScore(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Score})";
+        }
+    }
+}
---
+++ ScoreboardLibrary/Scoreboard.cs
@@ -0,0 +1,33 @@
+﻿namespace ScoreboardLibrary
+{
+    public class Scoreboard
+    {
+        private readonly List<PlayerScore> scoreList = new();
+
+        public List<PlayerScore> PlayerScores { get => scoreList; }
+
+        public Scoreboard()
+        {
+        }
+
+        public void AddPlayer(PlayerScore player)
+        {
+            scoreList.Add(player);
+        }
+
+        public void AddPlayer(string name, int score)
+        {
+            scoreList.Add(new PlayerScore(name, score));
+        }
+
+        public override string ToString()
+        {
+            return $"Scoreboard: {string.Join(", ", scoreList)}";
+        }
+
+        public void SortScoreBoard()
+        {
+            scoreList.Sort();
+        }
+    }
+}
