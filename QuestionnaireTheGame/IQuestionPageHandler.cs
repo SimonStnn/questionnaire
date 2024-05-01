89ec8617ddb9e836becdaed936de5cc7f2fd452d
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-01 17:27:27 +0200

    Add IQuestionPageHandler interface

---
+++ QuestionnaireTheGame/IQuestionPageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuestionnaireLibrary;
+
+namespace QuestionnaireTheGame
+{
+    public interface IQuestionPageHandler
+    {
+        public Question CurrentQuestion { get; }
+        public List<Question> Questions { get; }
+        public List<Answer> Guesses { get; }
+
+        public void ProcessQuestion(Question question);
+
+        public void QuestionAnswered(Answer answer);
+    }
+}
