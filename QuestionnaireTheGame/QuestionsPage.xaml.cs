767298b47657b0d6a9d3f7d80b897fc0bcc0715b
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-14 11:38:29 +0200

    Fix current question bug

--- QuestionnaireTheGame/QuestionsPage.xaml.cs
+++ QuestionnaireTheGame/QuestionsPage.xaml.cs
@@ -16,57 +16,58 @@ using System.Windows.Shapes;
 
 namespace QuestionnaireTheGame
 {
-    /// <summary>
-    /// Interaction logic for QuestionsPage.xaml
-    /// </summary>
-    public partial class QuestionsPage : Page
-    {
-        public readonly IQuestionPageHandler handler;
+	/// <summary>
+	/// Interaction logic for QuestionsPage.xaml
+	/// </summary>
+	public partial class QuestionsPage : Page
+	{
+		public readonly IQuestionPageHandler handler;
 
-        public QuestionsPage(IQuestionPageHandler handler)
-        {
-            this.handler = handler;
+		public QuestionsPage(IQuestionPageHandler handler)
+		{
+			this.handler = handler;
 
-            InitializeComponent();
-        }
-        public void RenderQuestion()
-        {
-            RenderQuestion(handler.CurrentQuestionIndex);
-        }
-        public void RenderQuestion(int index)
-        {
-            RenderQuestion(handler.Questions[index]);
-        }
-        public void RenderQuestion(Question question)
-        {
-            tbQuestion.Text = question.Text;
-            spAnswers.Children.Clear();
-            foreach (Answer answer in question.Answers)
-            {
-                Button btn = RenderAnswer(answer);
-                spAnswers.Children.Add(btn);
-            }
-        }
+			InitializeComponent();
+		}
+		public void RenderQuestion()
+		{
+			RenderQuestion(handler.CurrentQuestionIndex);
+		}
+		public void RenderQuestion(int index)
+		{
+			RenderQuestion(handler.Questions[index]);
+		}
+		public void RenderQuestion(Question question)
+		{
+			tbQuestion.Text = question.Text;
+			spAnswers.Children.Clear();
+			foreach(Answer answer in question.Answers)
+			{
+				Button btn = RenderAnswer(answer);
+				spAnswers.Children.Add(btn);
+			}
+		}
 
-        public Button RenderAnswer(Answer answer)
-        {
-            Button btn = new()
-            {
-                Content = answer.Text,
-            };
-            btn.Click += (sender, e) =>
-            {
-                handler.QuestionAnswered(answer);
-                if (++handler.CurrentQuestionIndex < handler.Questions.Count)
-                {
-                    RenderQuestion();
-                }
-                else
-                {
-                    handler.Done();
-                }
-            };
-            return btn;
-        }
-    }
+		public Button RenderAnswer(Answer answer)
+		{
+			Button btn = new()
+			{
+				Content = answer.Text,
+			};
+			btn.Click += (sender, e) =>
+			{
+				if(++handler.CurrentQuestionIndex < handler.Questions.Count)
+				{
+					handler.QuestionAnswered(answer);
+					RenderQuestion();
+				}
+				else
+				{
+					handler.QuestionAnswered(answer);
+					handler.Done();
+				}
+			};
+			return btn;
+		}
+	}
 }
